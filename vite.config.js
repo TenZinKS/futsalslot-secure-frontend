c62714c80fb8b8f87f3dea5974c75ec04d866b05
TenZinKS <tenzinkuenzel2016@gmail.com>
2026-01-27 17:09:52 +0545

    Configured HTTPS for frontend

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "node:fs";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync("certs/localhost-key.pem"),
+      cert: fs.readFileSync("certs/localhost.pem"),
+    },
+  },
   build: {
     sourcemap: false,
   },
-})
+});
